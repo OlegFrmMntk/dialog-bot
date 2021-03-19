045583e1ace78b7424a72fc77b7e0b86a085b251
OlegFrmMntk <Mankevich000@mail.ru>
2021-03-19 12:13:44 +0300

    add help

--- SemanticLanguageBot.py
+++ SemanticLanguageBot.py
@@ -2,6 +2,21 @@ import telebot
 
 import semantic_handler
 
+help_message = """
+1)Choose a command
+2)Enter the text (Send the document for command "Build word cloud")
+
+Dictionary:
+S -- Start element(whole sentence)
+P -- Preposition
+N -- Noun
+V -- Verbs and modals
+JJP -- Adjective part(Adjectives, adverbs and cardinals)
+NP -- Noun part
+PP -- Prepositional part
+VP -- Verbal part
+"""
+
 # You can set parse_mode by default. HTML or MARKDOWN
 bot = telebot.TeleBot('1734276898:AAFzgB1ShRrZ_jd3AbrKls51QC4O8soK0_Y', parse_mode=None)
 bot_command_key = ''
@@ -17,10 +32,17 @@ def send_text(message):
     global bot_command_key
     key = False
 
-    if message.text.lower() in ('add text to the dictionary', 'build semantic tree', 'get semantic analyse of text'):
+    if message.text.lower() == 'help':
+        key = True
+        bot.send_message(message.chat.id, help_message)
+    elif message.text.lower() in ('add text to the dictionary', 'build semantic tree', 'get semantic analyse of text'):
         key = True
         bot_command_key = str(message.text.lower())
         bot.send_message(message.chat.id, 'Enter a text')
+    elif message.text.lower() == 'build word cloud':
+        key = True
+        bot_command_key = str(message.text.lower())
+        bot.send_message(message.chat.id, 'Send the document')
 
     if not key and len(bot_command_key) > 0:
 
@@ -31,6 +53,8 @@ def send_text(message):
             bot.send_photo(message.chat.id, semantic_handler.build_syntax_tree(message.text))
         elif bot_command_key == 'get semantic analyse of text':
             bot.send_message(message.chat.id, 'Semantic analyse')
+        elif bot_command_key == 'build word cloud':
+            bot.send_photo(message.chat.id, open('squirtle.png', 'rb'))
 
         bot_command_key = ''
     elif not key:
@@ -42,4 +66,5 @@ keyboard = telebot.types.ReplyKeyboardMarkup()
 keyboard.row('Add text to the dictionary')
 keyboard.row('Build semantic tree')
 keyboard.row('Get semantic analyse of text')
+keyboard.row('Build word cloud')
 keyboard.row('Help')
