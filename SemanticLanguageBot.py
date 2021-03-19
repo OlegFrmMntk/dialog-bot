76351b993b8b48f7aac3b348ccde8b2ea3269f5f
OlegFrmMntk <Mankevich000@mail.ru>
2021-03-20 01:05:34 +0300

    Fix bug for multiple users

--- SemanticLanguageBot.py
+++ SemanticLanguageBot.py
@@ -19,7 +19,7 @@ VP -- Verbal part
 
 # You can set parse_mode by default. HTML or MARKDOWN
 bot = telebot.TeleBot('1734276898:AAFzgB1ShRrZ_jd3AbrKls51QC4O8soK0_Y', parse_mode=None)
-bot_command_key = ''
+bot_command_key = dict()
 
 
 @bot.message_handler(commands=['start'])
@@ -34,7 +34,8 @@ def help_message_handler(message):
 
 @bot.message_handler(content_types=["document"])
 def content_document(message):
-    bot.send_photo(message.chat.id, semantic_handler.generate_wordcloud(message))
+    bot.send_photo(message.chat.id, open('squirtle.png', 'rb'))
+    #bot.send_photo(message.chat.id, semantic_handler.generate_wordcloud(message))
 
 
 @bot.message_handler(content_types=['text'])
@@ -44,25 +45,25 @@ def send_text(message):
 
     if message.text.lower() in ('add text to the dictionary', 'build semantic tree', 'get semantic analyse of text'):
         key = True
-        bot_command_key = str(message.text.lower())
+        bot_command_key[message.chat.id] = str(message.text.lower())
         bot.send_message(message.chat.id, 'Enter a text')
     elif message.text.lower() == 'build word cloud':
         key = True
-        bot_command_key = str(message.text.lower())
+        bot_command_key[message.chat.id] = str(message.text.lower())
         bot.send_message(message.chat.id, 'Send the document')
 
-    if not key and len(bot_command_key) > 0:
+    if not key and message.chat.id in bot_command_key and len(bot_command_key[message.chat.id]) > 0:
 
-        if bot_command_key == 'add text to the dictionary':
+        if bot_command_key[message.chat.id] == 'add text to the dictionary':
             bot.send_message(message.chat.id, semantic_handler.tag_text(message.text))
-        elif bot_command_key == 'build semantic tree':
+        elif bot_command_key[message.chat.id] == 'build semantic tree':
             bot.send_message(message.chat.id, 'TREEEEEEEEEEEEEEEEEEEEEE')
             semantic_handler.build_syntax_tree(message.text)
 #           bot.send_photo(message.chat.id, semantic_handler.build_syntax_tree(message.text))
-        elif bot_command_key == 'get semantic analyse of text':
+        elif bot_command_key[message.chat.id] == 'get semantic analyse of text':
             bot.send_message(message.chat.id, semantic_handler.analyze(message.text))
 
-        bot_command_key = ''
+        bot_command_key[message.chat.id] = ''
     elif not key:
         bot.send_message(message.chat.id, 'Chatting')
 
